c2ff3dbdce46243f7af2b1ccc21b0388a9a6523f
tyrax7 <lucaslucas34000@gmail.com>
2026-06-25 22:19:07 +0200

    fix

--- backend/src/admin/admin.js
+++ backend/src/admin/admin.js
@@ -11,13 +11,13 @@ async function setupAdmin(app) {
     Resource
   });
 
-  const db = await new Adapter('mysql2', {
-    host: 'localhost',
-    port: 3306,
-    user: 'root',
-    password: '',
-    database: 'jobiz_db'
-  }).init();
+const db = await new Adapter('mysql2', {
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT || 3306),
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
+}).init();
 
   const admin = new AdminJS({
     rootPath: '/admin',
